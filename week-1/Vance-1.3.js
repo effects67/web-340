9f8353691280fe6e75f297e818062d5077380f14
effects67 <32003168+effects67@users.noreply.github.com>
2018-02-24 19:54:20 -0600

    updating 1.3

--- week-1/Vance-1.3.js
+++ week-1/Vance-1.3.js
@@ -1,7 +1,16 @@
+var header = require('../header.js');
+
+console.log(header.display("Exercise 1.3", "Gabriel", "Vance", "Parses URL")); 
+
+
+
 var url = require("url");
 
+// var parsed URL to personal GitHub
 var parsedURL = url.parse("https://github.com?name=vance");
 
+
+// calls 3 seperate parsedURL
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 console.log(parsedURL.query);
