0f9d666d64724de6fc92df453f04c411272a3702
effects67 <32003168+effects67@users.noreply.github.com>
2018-03-14 18:14:56 -0500

    fixed week 4

--- week-4/status-code/Vance-4.3.js
+++ week-4/status-code/Vance-4.3.js
@@ -1,20 +1,32 @@
+var header = require('../../header.js');
+
+console.log(header.display("Exercise 4.3", "Gabriel", "Vance", "Status-Code")); 
+
+
+
+//while updating status codes can be done, you need to have a REALLY good reason for doing so
+
 var express=require("express");
 var http=require("http");
+var logger=require("morgan");
+
 var app=express();
+
+app.use(logger('dev'));
 
 app.get("/not-found",function(request,response){
     response.status(404);
     response.json({
         error:"404, not found!"
 
-    })
+    });
 });
 app.get("/ok",function(request,response){
     response.status(200);
     response.json({
-        message:"Everything worked fine!"
+        error:"Everything worked fine!"
 
-    })
+    });
 });
 
 app.get("/not-implemented",function(request,response){
