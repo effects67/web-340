44cd3037ec24bd2383dbd4cce706cbda7b7114e4
effects67 <32003168+effects67@users.noreply.github.com>
2018-04-08 20:28:37 -0500

    week-7

--- ems/app.js
+++ ems/app.js
@@ -1,18 +1,39 @@
 var header = require('../header.js');
 console.log(header.display("Assignment 6.4", "Gabriel", "Vance", "User Interface Submission")); 
 
-
+//required statements
 var express=require("express");
 var http=require("http");
 var path=require("path");
 var logger=require("morgan");
 
+var mongoose = require("mongoose");
+var Employee = require("./models/employee");
+
+//adding the DB
+
+var mongoDB="mongodb://charlie:brown@dbh42.mlab.com:27427/ems";
+mongoose.connect(mongoDB,{
+    useMongoClient:true
+});
+//mongo promises
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+});
+
+
+// expressapplication 
 var app=express();
 
+app.use(logger("short"));
+
+// set statements
 app.set("views",path.resolve(__dirname,"views"));
 app.set("view engine","ejs");
 
-app.use(logger("short"));
 
 app.get("/",function(request,response){
     response.render("index",{
@@ -31,6 +52,14 @@ app.get("/contact",function(request,response){
         message:"contact page"
     });
 });
+
+// employee model
+
+var employee=new Employee({
+    firstname:"John",
+    lastname:"Wick"
+});
+
 
 
 
---
+++ ems/models/employee.js
@@ -0,0 +1,40 @@
+/*
+INSTRUCTIONS:
+
+Create a new subdirectory in the ems home directory and name it models
+Install Mongoose (npm install mongoose --save)
+Create a new JavaScript file and name it employee.js and add it to the models directory
+
+Following the courses repositories fms fruit.js example, create an employeeSchema with two (2) fields, first name and last name
+
+Map the employeeSchema to an Employee model
+
+Make the module accessible from other JavaScript files (module.exports = Employee;)
+Update the app.js file with your mLab connection string, mongoose connection code, and Employee model
+Run and test the server in a web browser
+
+*/
+
+
+// Following the courses repositories fms fruit.js example, 
+
+//required and schema
+
+var mongoose=require("mongoose");
+var Schema=mongoose.Schema;
+
+//create an employeeSchema with two (2) fields, first name and last name
+
+var employeeSchema=new Schema({
+    firstname:String,
+    lastname:String,
+});
+
+//employee model added, maps the schema
+
+var Employee=mongoose.model("Employee",employeeSchema);
+
+//exports models
+
+module.exports=Employee;
+
