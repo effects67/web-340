7fcf3689aa87e67d886be2856a864e9113305bec
effects67 <32003168+effects67@users.noreply.github.com>
2018-04-17 08:39:26 -0500

    updating ems

--- ems/app.js
+++ ems/app.js
@@ -13,23 +13,19 @@ var header = require('../header.js');
 console.log(header.display("EMS", "Gabriel", "Vance", "User Interface Submission")); 
 
 //required statements
-var express=require("express");
-var http=require("http");
-var path=require("path");
-var logger=require("morgan");
-// add mongoose - week 8
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+//adding helmet for week 8
+var helmet = require("helmet");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 var mongoose = require("mongoose");
-var Employee = require("./models/employee");
-// add helmet - week 8
-var helmet=require("helmet");
-// add parsers - week 8
-var bodyParser=require("body-parser");
-var cookieParser=require("cookie-parser");
-var csrf=require("csurf");
-
+var employee = require("./models/employee");
 //setup csrf protection - week 8
 var csrfProtection=csrf({cookie:true});
-
 //adding the DB
 
 var mongoDB="mongodb://charlie:brown@dbh42.mlab.com:27427/ems";
@@ -52,23 +48,25 @@ var app=express();
 app.use(logger("short"));
 app.use(helmet.xssFilter());
 //parser week 8 - CSRF
+
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
 app.use(cookieParser());
+app.use(helmet.xssFilter());
 app.use(csrfProtection);
-app.use(function(request,response, next){
-    var token=request.csrfToken();
-    response.cookie('XSRF-TOKEN',token);
-    response.locals.csrfToken=token;
+app.use(function(request, response, next) {
+    var token = request.csrfToken();
+    response.cookie('XSRF-TOKEN', token);
+    response.locals.csrfToken = token;
     next();
 });
 
 
 // set statements
-app.set("views",path.resolve(__dirname,"views"));
+app.set("views",path.resolve(__dirname, "views"));
 app.set("view engine","ejs");
-app.set("port", process.env.PORT || 8080);
+app.set("port",process.env.PORT || 8080);
 
 
 // http calls week -8
@@ -105,37 +103,59 @@ app.post("/process", function(request, response) {
         return;
     }
 
-});
-//form data
+// employee model
 
-var employeeName = request.body.txtName;
+   // get the request's form data
+   var employeeName = request.body.txtName;
    console.log(employeeName);
+
+
+   var employee = new Employee({
+       name: employeeName
+   });
 //saves the employee input
 
 employee.save(function (error) {
     if (error) throw error;
 
-    console.log(employeeName + " saved successfully!");
+    console.log(employeeName  + " saved successfully!");
 });
 
+response.redirect("/");
+});
 
 app.get("/list", function(request, response) {
     employee.find({}, function(error, employees) {
        if (error) throw error;
 
        response.render("list", {
-           title: "employee List",
+           title: "Employee List",
            employees: employees
        });
     });
 });
 
-// employee model
+//query names
+app.get("/view/:queryName", function (request, response) {
+    var queryName = request.params.queryName;
 
-var employee=new Employee({
-    firstname:"John",
-    lastname:"Wick"
+    Employee.find({'name': queryName}, function(error, employees) {
+        if (error) throw error;
+
+        console.log(employees);
+
+        if (employees.length > 0) {
+            response.render("view", {
+                title: "Employee Record",
+                employee: employees
+            })
+        }
+        else {
+            response.redirect("/list")
+        }
+    });
 });
+
 
 
 
